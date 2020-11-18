ef453aa4cf15e59b27598f0e3b2f183d36bb1fb4
Melody Su <melodysu83@gmail.com>
2020-11-18 10:21:32 -0500

    improve and comment visualize.py

--- visualize.py
+++ visualize.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+# demonstrate the progress ogf generationg dataset
 class Progress_viz:
+
     def __init__(self, total):
         self.counter = 0
         self.total = total
+
     def __call__(self, trajectory):
         if not trajectory.is_boundary():
             self.counter += 1
@@ -14,12 +17,11 @@ class Progress_viz:
             print("\r{}/{}".format(self.counter, self.total), end="")
 
 
-def column(matrix, i):
-    return [row[i] for row in matrix]
-
+# demonstrate performance during the training phase
 def metrics_viz(all_metrics, all_train_loss):
-	avg_return_trained = column(all_metrics,0)
-	avg_ep_trained = column(all_metrics,1)
+
+	avg_return_trained = [row[0] for row in all_metrics]
+	avg_ep_trained = [row[1] for row in all_metrics]
 
 
 	fig, axs = plt.subplots(1, 2, figsize=(15,15))
@@ -35,14 +37,36 @@ def metrics_viz(all_metrics, all_train_loss):
 
 	plt.show()
 
-
+# demonstrate performance during the evaluation phase
 def observation_viz(observation, step=None, act=None, epi=None):
+	
 	if step == None:
 	    step = 0
-	    print("---\nStep: {}".format(step))
+	    print("------------------------\nStep: {}".format(step))
 	else:
-	    print("---\nStep: {}".format(step))
-	    print("Action taken: {}".format(act))
+	    print("------------------------\nStep: {}".format(step))
+	    if act < 8:
+	        string_act = " 🐕 "
+	    else:
+	    	string_act = " 🐈 "
+	    if act == 0 or act == 8:
+	    	string_act = string_act + " moves left."
+	    elif act == 1 or act == 9:
+	    	string_act = string_act + " moves right."
+	    elif act == 2 or act == 10:
+	    	string_act = string_act + " moves down."
+	    elif act == 3 or act == 11:
+	    	string_act = string_act + " moves up."
+	    elif act == 4 or act == 12:
+	    	string_act = string_act + " attacks top right."
+	    elif act == 5 or act == 13:
+	    	string_act = string_act + " attacks top left."
+	    elif act == 6 or act == 14:
+	    	string_act = string_act + " attacks bottom right."
+	    else:
+	    	string_act = string_act + " attacks bottom left."
+
+	    print("Action taken: {}".format(act)+string_act)
 	    print("Reward: {} \n".format(epi))
 
 	numpy_obs = observation.numpy()[0]
